52854bd32dd8391de79c10b51006b3b3ca373391
Andrey Ognevsky <ognevsky@gmail.com>
2009-04-16 16:47:24 +0200

    mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,14 +17,14 @@ config.action_controller.perform_caching             = false
 config.action_mailer.raise_delivery_errors = true
 
 # set delivery method to :smtp, :sendmail or :test
-config.action_mailer.delivery_method = :sendmail
+config.action_mailer.delivery_method = :smtp
 
 # these options are only needed if you choose smtp delivery
-# config.action_mailer.smtp_settings = {
-#   :address        => 'smtp.example.com',
-#   :port           => 25,
-#   :domain         => 'www.example.com',
-#   :authentication => :login,
-#   :user_name      => 'www',
-#   :password       => 'secret'
-# }
+config.action_mailer.smtp_settings = {
+  :address        => 'localhost',
+  :port           => 25,
+  :domain         => 'dressinfun.com',
+  :authentication => :plain,
+  :user_name      => '',
+  :password       => ''
+}
